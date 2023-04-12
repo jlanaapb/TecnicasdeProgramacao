a756b075109ae4b1a793ef9dc7fca57e06be7dff
Juliana Barboza <108034424+jlanaapb@users.noreply.github.com>
2023-04-12 10:15:01 -0300

    lista 3, peimeiro commit
    1 versão da lista 3

---
+++ Lista03/src/questoes/questao01.java
@@ -0,0 +1,32 @@
+package questoes;
+
+public class questao01 {
+
+	public static int somatorio(int vetorum[]){
+		int soma=0;
+		for(int a=0;a<(vetorum.length-1);a++)
+			soma= soma+vetorum[a];
+		
+		return soma;
+	}
+	
+	public static int[] povoarvetor(int vetorum[]) {
+		int b=0;
+		for(b=0;b<(vetorum.length-1);b++) {
+			vetorum[b]=b;
+			System.out.println(vetorum[b]);}
+			
+		return vetorum;
+	}
+	
+	public static void main(String[] args) {
+
+		int vetorum[]= new int[5];
+		
+		povoarvetor(vetorum);
+		System.out.println("Soma: ");
+		System.out.println(somatorio(vetorum));
+		
+	}
+
+}
