594972bffbd4f8b3d99dbddecfe40eced5554235
Juliana Barboza <108034424+jlanaapb@users.noreply.github.com>
2023-04-13 10:12:21 -0300

    Add files via upload
    lista3, 2 commit

---
+++ Lista03/src/questoes/Quest03.java
@@ -0,0 +1,51 @@
+package questoes;
+
+import java.util.Scanner;
+
+public class Quest03 {
+
+	
+	public static int weightedAverage(int[] vectorOne, int[] vectorTwo) {
+//		int sumOne=0;
+//		for(int i=0; i<vectorOne.length; i++) {
+//			sumOne+= vectorOne[i];
+//		}
+//		sumOne=sumOne/5;
+//		
+//		
+//		int sumTwo=0;
+//		for(int i=0; i<vectorTwo.length; i++) {
+//			sumTwo+= vectorTwo[i];
+//		}
+//		sumTwo=sumTwo/5;
+//		
+//		int mediaTotal;
+//		
+//		mediaTotal=(sumOne+sumTwo)/2;
+//		
+//		
+		//return mediaTotal;
+	}
+	
+	
+	public static void main(String[] args) {
+		int grades[]= new int[5];
+		int value[]= new int[5];
+		
+		grades[0]= 8;
+		grades[1]= 9;
+		grades[2]= 10;
+		grades[3]= 7;
+		grades[4]= 5;
+		
+		value[0]= 1;
+		value[1]= 3;
+		value[2]= 1;
+		value[3]= 8;
+		value[4]= 2;
+		
+		
+		
+	}
+
+}
