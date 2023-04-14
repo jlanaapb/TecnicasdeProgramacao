7fbb03d11555c5526cd6514b7438bef0e8a4b2b6
Juliana Barboza <108034424+jlanaapb@users.noreply.github.com>
2023-04-14 12:10:50 -0300

    lista 3
    terceira atualização

---
+++ Lista03/src/questoes/Lista03/src/questoes/Quest04.java
@@ -0,0 +1,58 @@
+package questoes;
+import java.util.Scanner;
+
+public class Quest04 {
+	
+	public static void countElements(float[] vectorOne,float[] vectorTwo) {
+		Scanner input= new Scanner(System.in);
+		int b=0;
+		int quantidade=0;
+		
+		System.out.println(" Qual elemento deve ser buscado?");
+		float element = input.nextInt();
+	
+		
+		for(int a=0;a<vectorOne.length;a++) {
+			
+			if(vectorOne[a]==element) {
+				b++;
+				quantidade+=b;}
+			
+		}
+
+		for(int a=0;a<vectorTwo.length;a++) {
+			if(vectorTwo[a]==element) {
+				b++;
+				quantidade+=b;}
+			
+		}
+		System.out.println("o elemento "+element+" apareceu "+b+" vezes");
+	}
+	
+	public static void initiate() {
+		Scanner input= new Scanner(System.in);
+		float vectorOne[]= new float[5];
+		float vectorTwo[]= new float[5];
+		
+			System.out.println("Digite os valores do primeiro vetor: ");
+				for(int a=0; a<vectorOne.length;a++) {
+					System.out.println("indice "+a+":");
+					vectorOne[a]=input.nextInt();
+				}
+		
+			System.out.println("Digite os valores do segundo vetor: ");
+				for(int b=0; b<vectorTwo.length;b++) {
+					System.out.println("indice "+b+":");
+					vectorTwo[b]=input.nextInt();
+				}
+			
+				countElements(vectorOne, vectorTwo);
+	}
+	public static void main(String[] args) {
+		initiate();
+		
+		
+		
+	}
+
+}
