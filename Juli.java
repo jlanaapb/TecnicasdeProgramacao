f8102704920b8adc9250cf40028c661c761217fc
Juliana Barboza <108034424+jlanaapb@users.noreply.github.com>
2023-07-11 19:21:08 -0300

    _Robocode

--- Juli.java
+++ Juli.java
@@ -1,34 +1,40 @@
 package tecProg;
-
 import java.awt.Color;
+
 import robocode.AdvancedRobot;
 import robocode.HitByBulletEvent;
 import robocode.HitRobotEvent;
 import robocode.HitWallEvent;
 import robocode.ScannedRobotEvent;
 
-public class Juliana extends AdvancedRobot {
-
+public class Juli extends AdvancedRobot{
+	
 	public void run() {
-		setColors(Color.MAGENTA, Color.cyan, Color.GREEN);
+		setColors(Color.magenta, Color.ORANGE, Color.YELLOW);
 
 		while (true) {
-			setAhead(50);
-			setTurnLeft(25);
+			setAhead(25);
 			setTurnGunLeft(50);
-			turnRadarLeft(360);
+			setTurnLeft(25);
+			turnRadarLeft(180);
 			turnRadarRight(180);
 			setTurnRight(50);
-			setAhead(70);
+			setAhead(200);
 		}
 	}
 
+	public void onHitWall(HitWallEvent event) {
+		turnLeft(90);
+		turnRadarLeft(360);
+		setAhead(200);
+	}
 	public void onScannedRobot(ScannedRobotEvent e) {
 		double angulo = e.getBearing();
 		double distancia = e.getDistance();
 
 		if (distancia > 50) {
 			turnGunRight(angulo);
+			fire(2);
 			setBack(distancia);
 			fire(2);
 		}
@@ -36,25 +42,12 @@ public class Juliana extends AdvancedRobot {
 		if (distancia <= 50) {
 			setStop();
 			turnGunRight(angulo);
+			fire(3);
 			setBack(distancia);
 			fire(3);
 		}
 
 	}
-	public void onHitWall(HitWallEvent event) {
-		back(100);
-		turnLeft(180);
-		turnRadarLeft(180);
-		ahead(100);
-	}
-
-	public void onHitByBullet(HitByBulletEvent e) {
-		setAhead(100);
-		setTurnRight(50);
-		setAhead(100);
-		
-	}
-
 	public void onHitRobot(HitRobotEvent event) {
 		double angulo = event.getBearing();
 		if (angulo > 50) {
@@ -69,4 +62,13 @@ public class Juliana extends AdvancedRobot {
 			fire(3);
 		}
 	}
+
+	public void onHitByBullet(HitByBulletEvent e) {
+		setAhead(100);
+		setTurnRight(50);
+		setAhead(100);
+	}
+
+
+	
 }
